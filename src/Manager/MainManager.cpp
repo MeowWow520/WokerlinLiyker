b8352428260fcf1dc4275424f06eed696b6e3f23
MeowWow520 <Meow114514@outlook.com>
2026-01-06 15:47:53 +0800

    Created Window

--- src/Manager/MainManager.cpp
+++ src/Manager/MainManager.cpp
@@ -3,25 +3,57 @@
 
 
 MainManager::MainManager() {
-    SetLoggerLevel(spdlog::level::warn);
+    SetLoggerLevel(spdlog::level::info);
     spdlog::info("MainManager initialized.");
+
+    MainManager::AssertionFailure(!SDL_Init(SDL_INIT_EVERYTHING), u8"SDL");
+    MainManager::AssertionFailure(!TTF_Init(), u8"SDL_ttf");
+    MainManager::AssertionFailure(Mix_Init(MIX_INIT_MP3), u8"SDL_mixer");
+    MainManager::AssertionFailure(IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG), u8"SDL_image");
+    // 打开音频，设置音频格式
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+	SDLWindow = SDL_CreateWindow(u8"WokerlinLiyker's Application", 
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+			WindowWidth, WindowHeight, SDL_WINDOW_SHOWN);
+    
+	MainManager::AssertionFailure(SDLWindow, u8"SDL Window");
+	SDLRenderer = SDL_CreateRenderer(SDLWindow, -1, 
+		SDL_RENDERER_ACCELERATED | 
+        SDL_RENDERER_PRESENTVSYNC | 
+        SDL_RENDERER_TARGETTEXTURE);
+	MainManager::AssertionFailure(SDLRenderer, u8"SDL Renderer");
+
 }
 
 MainManager::~MainManager() {
+    SDL_DestroyRenderer(SDLRenderer);
+	SDL_DestroyWindow(SDLWindow);
+	TTF_Quit();
+	Mix_Quit();
+	IMG_Quit();
+	SDL_Quit();
     spdlog::info("MainManager destroyed.");
+
 }
 
 int MainManager::RunApplication(int argc, char** argv) {
     (void)argc;
     (void)argv;
-    spdlog::info("Application Started.");
     // Main application loop
+    spdlog::info("Application Started.");
+    while (GetIsRunning()) {
+        MainManager::SDLPullEvents(SDLEvent);
+        
+    }
     return 0;
 }
 
 void MainManager::Shutdown() {
-    spdlog::info("Application Shutting Down.");
-    IsRunning = false;
+    if (GetIsRunning()) {
+        spdlog::info("Application Shutting Down.");
+        IsRunning = false;
+    }
 }
 
 bool MainManager::SetLoggerLevel(spdlog::level::level_enum level) {
@@ -31,5 +63,38 @@ bool MainManager::SetLoggerLevel(spdlog::level::level_enum level) {
     } catch (const spdlog::spdlog_ex& ex) {
         spdlog::error("Log level set failed: {}", ex.what());
         return false;
+    }
+}
+
+void MainManager::AssertionFailure(bool Condition, const char* Message) {
+    if (Condition) 
+        spdlog::info("Assertion passed. {} loaded successfully.", Message);
+    else 
+        spdlog::error("Assertion failed! {} failed to load.", Message);
+}
+
+void MainManager::PrintPrivateVariables() const {
+    spdlog::trace("IsRunning: {}", IsRunning);
+    spdlog::trace("WindowWidth: {}", WindowWidth);
+    spdlog::trace("WindowHeight: {}", WindowHeight);
+    spdlog::trace("IsPrintMouseMove: {}", IsPrintMouseMove);
+}
+
+void MainManager::SDLPullEvents(SDL_Event& Event) {
+    while (SDL_PollEvent(&Event)) {
+        switch (Event.type) {
+        case SDL_QUIT:
+            spdlog::info("SDL_QUIT event received. Exiting main loop.");
+            SetIsRunning(false);
+            break;
+        case SDL_MOUSEMOTION:
+            PosCursor.x = Event.motion.x;
+            PosCursor.y = Event.motion.y;
+            if (IsPrintMouseMove)
+                spdlog::trace("Mouse moved to ({}, {})", Event.motion.x, Event.motion.y);
+            break;
+        default:
+            break;
+        }
     }
 }
--- src/Manager/MainManager.h
+++ src/Manager/MainManager.h
@@ -3,6 +3,11 @@
 #ifndef MAINMANAGER_H
 #define MAINMANAGER_H
 #include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #include "SITemplate.h"
 #include "spdlog/spdlog.h"
 
@@ -11,6 +16,7 @@ class MainManager : public SITemplate<MainManager> {
     friend class SITemplate<MainManager>;
 
 public:
+    // 构造函数与析构函数
     MainManager();
     ~MainManager();
     // 主应用程序运行函数
@@ -20,10 +26,29 @@ public:
 protected:
     // 关闭应用程序
     void Shutdown();
-    // 设置日志记录级别
+    // 设置日志输出级别
     bool SetLoggerLevel(spdlog::level::level_enum level);
+
+    void AssertionFailure(bool Condition, const char* Message);
+    bool GetIsRunning() const { return IsRunning; }
+    void SetIsRunning(bool running) { IsRunning = running; }
+    void PrintPrivateVariables() const;
+    void SDLPullEvents(SDL_Event& Event);
+
 private:
-    bool IsRunning = true;
+    // 主循环控制变量
+    bool IsRunning{true};
+    // 是否打印鼠标移动事件
+    bool IsPrintMouseMove{false};
+    // 窗口尺寸 - 宽
+    int WindowWidth{1280};
+    // 窗口尺寸 - 高
+    int WindowHeight{720};
+    SDL_Event SDLEvent;
+	SDL_Point PosCursor = { 0,0 };
+    SDL_Window* SDLWindow = nullptr;
+    SDL_Renderer* SDLRenderer = nullptr;
+
 };
 
 #endif // !MAINMANAGER_H
