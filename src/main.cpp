89cf9136f028597e9ed65ec70bb1b33c9b343558
MeowWow520 <Meow114514@outlook.com>
2026-01-06 13:37:15 +0800

    Delete all mistakes in defind header

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #define SDL_MAIN_HANDLED
 #define _CRT_SECURE_NO_WARNINGS
-#include "Manager/MainManager.h"
 
 
 
 int main(int argc, char** argv) 
 {
-    return MainManager::Instance()->StartApplication(argc, argv);
+    (void)argc;
+    (void)argv;
+    return 0;
 }
